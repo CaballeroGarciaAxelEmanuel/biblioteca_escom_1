1f05afb91aa4b1e8bd971c7318b0ec86c728207b
CaballeroGarciaAxelEmanuel <axelwila2002@gmail.com>
2026-01-06 18:48:22 -0600

    Configurar puerto con process.env.PORT

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const configuracionRoutes = require('./routes/configuracion');
 const app = express();
-const PORT = 3000;
+const PORT process.env.PORT = 3000;
 
 /* ===== CORS (primero) ===== */
 app.use(cors({
